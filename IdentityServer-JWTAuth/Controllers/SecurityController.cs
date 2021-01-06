47dd1ef5c555728529179ab7e627b487a636d8c6
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-01-06 20:24:55 +0500

    added Home controller for authorization

--- IdentityServer-JWTAuth/Controllers/SecurityController.cs
+++ IdentityServer-JWTAuth/Controllers/SecurityController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServer_JWTAuth.Controllers
@@ -11,11 +12,13 @@ namespace IdentityServer_JWTAuth.Controllers
     public class SecurityController : ControllerBase
     {
         [HttpPost]
+        [AllowAnonymous]
         public void Login([FromBody] string value)
         {
 
         }
         [HttpPost]
+        [AllowAnonymous]
         public void Signup([FromBody] string value)
         {
 
