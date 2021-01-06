9601ae2a3e2556426b4cb8e70b0da32c9aacec45
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-01-06 20:14:32 +0500

    Added Security Controller for login,signup

---
+++ IdentityServer-JWTAuth/Controllers/SecurityController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer_JWTAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        [HttpPost]
+        public void Login([FromBody] string value)
+        {
+
+        }
+        [HttpPost]
+        public void Signup([FromBody] string value)
+        {
+
+        }
+        public async Task<string> GenerateJWT()
+        {
+            return "";
+        }
+    }
+}
