76bf8e0f2bb9e6101c1d58f05d8f4d169ed2b57f
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-05-28 23:41:00 +0500

    Added Configuration for Jwt Token Authentication.

--- IdentityServer-JWTAuth/Startup.cs
+++ IdentityServer-JWTAuth/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityServer_JWTAuth
 {
@@ -23,6 +26,19 @@ namespace IdentityServer_JWTAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options=>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey=true,
+                    ValidIssuer=Configuration["Jwt:Issuer"],
+                    ValidAudience=Configuration["Jwt:Issuer"],
+                    IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
             services.AddRazorPages();
         }
 
