7df3401d4e4f91312f3d4edfdba9d97b9156fa9c
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-06-17 02:30:41 +0500

    Added Db context.

---
+++ DataAccessLayer/DataContext/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DataContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
