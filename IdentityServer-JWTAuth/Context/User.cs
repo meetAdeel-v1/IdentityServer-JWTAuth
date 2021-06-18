f8c6f722221d8f40f65558675db7d6fcacd1455c
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-06-19 01:35:21 +0500

    Identity User model is added.

---
+++ IdentityServer-JWTAuth/Context/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer_JWTAuth.Context
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
