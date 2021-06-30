f7def608e63f2a76b33b29eab7b2b26dc503f7b9
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-01 01:17:41 +0500

    Added datestamp model.

---
+++ IdentityServer-JWTAuth/Common/DateTimeStamp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer_JWTAuth.Common
+{
+    public class DateTimeStamp
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
