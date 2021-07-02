7e3b9036e748822253eeee0febeb45e5e0dc467b
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-03 00:26:19 +0500

    Added EmployeesService

---
+++ BusinessLogicLayer/Services/EmployeeService/EmployeeService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.EmployeeService
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public async Task<ActionResult> GetAllEmployees()
+        {
+            return null;
+        }
+
+        public async Task<ActionResult> GetEmployee()
+        {
+            return null;
+        }
+        public async Task<ActionResult> AddEmployee()
+        {
+            return null;
+        }
+
+        public async Task<ActionResult> UpdateEmployee()
+        {
+            return null;
+        }
+
+        public async Task<ActionResult> DeleteEmployee()
+        {
+            return null;
+        }
+    }
+}
