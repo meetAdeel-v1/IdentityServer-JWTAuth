94d371f8d3eaf5f5113f5c95571200425fc2862f
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-03 00:26:44 +0500

    Added EmployeeService dependency.

---
+++ BusinessLogicLayer/Services/EmployeeService/IEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.EmployeeService
+{
+    public interface IEmployeeService
+    {
+        Task<ActionResult> GetAllEmployees();
+
+        Task<ActionResult> GetEmployee();
+
+        Task<ActionResult> AddEmployee();
+
+        Task<ActionResult> UpdateEmployee();
+
+        Task<ActionResult> DeleteEmployee();
+    }
+}
