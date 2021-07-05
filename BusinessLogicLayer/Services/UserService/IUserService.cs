5532cc2da234570cff23c67ecdeafb71c056128c
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-05 20:14:13 +0500

    User dependency added.

---
+++ BusinessLogicLayer/Services/UserService/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.UserService
+{
+    public interface IUserService
+    {
+        Task<ActionResult> GetAllUsers();
+
+        Task<ActionResult> GetUser();
+
+        Task<ActionResult> AddUser();
+
+        Task<ActionResult> UpdateUser();
+
+        Task<ActionResult> DeleteUser();
+    }
+}
+
