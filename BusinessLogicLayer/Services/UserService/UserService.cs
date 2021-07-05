798cbcae7b7e3ea4207933f462f68872112e949b
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-05 20:13:40 +0500

    User Service added.

---
+++ BusinessLogicLayer/Services/UserService/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        public async Task<ActionResult> GetAllUsers()
+        {
+            return null;
+        }
+
+        public async Task<ActionResult> GetUser()
+        {
+            return null;
+        }
+        public async Task<ActionResult> AddUser()
+        {
+            return null;
+        }
+
+        public async Task<ActionResult> UpdateUser()
+        {
+            return null;
+        }
+
+        public async Task<ActionResult> DeleteUser()
+        {
+            return null;
+        }
+    }
+}
