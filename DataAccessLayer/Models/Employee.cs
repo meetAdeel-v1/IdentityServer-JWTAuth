af22ea2131961d94a5f39fe1f08d7d3dab08f634
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-07 02:28:42 +0500

    Added Employee Model in DAL

---
+++ DataAccessLayer/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer_JWTAuth.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer_JWTAuth.Models
+{
+    public class Employee: DateTimeStamp
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
