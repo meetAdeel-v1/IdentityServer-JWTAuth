9c774bd971bf9cb9caa473265a85f0ddbda953d1
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-08 02:47:28 +0500

    Added General Repository in DAL.

---
+++ DataAccessLayer/Repositories/Concrete/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+    {
+        public GenericRepository()
+        {
+
+        }
+
+        public async Task<T> Get(T dataObj)
+        {
+            return dataObj;
+        }
+        public async Task<T> GetAll(T dataObj)
+        {
+            return dataObj;
+        }
+        public async Task<T> Add(T dataObj)
+        {
+            return dataObj;
+        }
+
+        public async Task<T> Update(T dataObj)
+        {
+            return dataObj;
+        }
+
+        public async Task<T> Delete(T dataObj)
+        {
+            return dataObj;
+        }
+    }
+}
