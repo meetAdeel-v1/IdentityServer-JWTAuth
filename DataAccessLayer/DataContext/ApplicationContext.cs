7807acad5fafaab46afa5baa979b1d1adbb20267
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-08 02:46:38 +0500

    Added Application Context in DAL.

--- DataAccessLayer/DataContext/ApplicationContext.cs
+++ DataAccessLayer/DataContext/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.DataContext
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationContext : IdentityDbContext<User>
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
 
         }
 
-        public DbSet<User> Users { get; set; }
+        //public DbSet<User> Users { get; set; }
     }
 }
