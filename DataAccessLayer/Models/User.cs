4848e103daad350c18822a3aa5b35b0372f439aa
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-09 02:31:25 +0500

    User Model is added in DAL

--- DataAccessLayer/Models/User.cs
+++ DataAccessLayer/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Models
 {
-    public class User
+    public class User: IdentityUser
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        
     }
 }
