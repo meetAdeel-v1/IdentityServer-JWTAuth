56c256b93f3e1fd0b075ec725ffa085264d00951
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-12 22:59:48 +0500

    fixed namespace issue in login model.

--- DataAccessLayer/Models/Login.cs
+++ DataAccessLayer/Models/Login.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IdentityServer_JWTAuth.Models
+namespace DataAccessLayer.Models
 {
     public class Login
     {
