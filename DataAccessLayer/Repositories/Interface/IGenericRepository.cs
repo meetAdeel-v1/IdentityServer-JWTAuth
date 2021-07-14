0c5b554abd5981a0c731c66c0726a06c3d804869
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-14 23:55:08 +0500

    Added Generic Repository in DAL.

---
+++ DataAccessLayer/Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Interface
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> Get(T dataObj);
+
+        Task<T> GetAll(T dataObj);
+
+        Task<T> Add(T dataObj);
+
+        Task<T> Update(T dataObj);
+
+        Task<T> Delete(T dataObj);
+    }
+}
