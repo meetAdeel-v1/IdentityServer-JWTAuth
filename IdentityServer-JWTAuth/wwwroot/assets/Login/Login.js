91686cc1a66f41a6a98c4e45598b063750e1934f
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-17 01:51:07 +0500

    Added login validations.

---
+++ IdentityServer-JWTAuth/wwwroot/assets/Login/Login.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#btnLogin').click(function () {
+        ValidateForm();
+    })
+});
+
+function ValidateForm() {
+    if (!$('#loginForm').valid()) {
+        return false;
+    }
+}
