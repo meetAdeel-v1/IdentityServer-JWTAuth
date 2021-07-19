b9163063b58c958e2983685525aeb65061438471
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-19 23:55:52 +0500

    Added foregion key relationship.

--- DataAccessLayer/Models/Employee.cs
+++ DataAccessLayer/Models/Employee.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +10,16 @@ namespace DataAccessLayer.Models
 {
     public class Employee: DateTimeStamp
     {
+        [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
+
+        [ForeignKey("Department")]
+        public int DeptId { get; set; }
+        public Department Department { get; set; }
+
     }
 }
