c51cee732b4fc56164c45183d11b1d88325222a4
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-07-26 23:54:35 +0500

    Added Login Viewmodel.

---
+++ DataAccessLayer/ViewModels/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels
+{
+    public class Login
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the UserName/Email")]
+        [Display(Name ="Email/Username")]
+        public string Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the Password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
