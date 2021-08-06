ce64414086c3341c7dc1136f5faff2de97b5d272
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-06 23:04:15 +0500

    Added CommonController and logic for Single file upload.

---
+++ IdentityServer-JWTAuth/Controllers/CommonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer_JWTAuth.Controllers
+{
+    public class CommonController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       public async Task<IActionResult> fileUpload(IFormFile file)
+        {
+            long fileSize = file.Length;
+            var filePath = Path.GetTempFileName();
+            using (var stream = new FileStream(filePath,FileMode.Create)) 
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok();
+        }
+    }
+}
