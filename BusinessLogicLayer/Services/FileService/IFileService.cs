7366f36623676e43663b1d4b8efe5be322a53c18
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-07 01:36:20 +0500

    Added file service dependency.

---
+++ BusinessLogicLayer/Services/FileService/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.FileService
+{
+    public interface IFileService
+    {
+        Task<FileData> saveFile(FileData file);
+        Task<List<FileData>> saveFiles(List<FileData> files);
+        Task<FileData> getFile();
+        Task<FileData> getFiles();
+    }
+}
