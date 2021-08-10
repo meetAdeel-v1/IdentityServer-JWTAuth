14ec80b36569080e81c9174f99e4f59eb67568c3
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-10 23:58:18 +0500

    Added usersignup model.

--- IdentityServer-JWTAuth/Models/UserSignup.cs
+++ IdentityServer-JWTAuth/Models/UserSignup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace IdentityServer_JWTAuth.Models
 {
     public class UserSignup
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
+        [Required(ErrorMessage ="This Field is Required.")]
+        [Display(Name ="Test")]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
     }
 }
