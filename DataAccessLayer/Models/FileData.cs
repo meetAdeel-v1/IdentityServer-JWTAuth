9252587ec4e0cc8561c10330dd21ee10883ad3e9
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-12 23:58:51 +0500

    Added file model.

---
+++ DataAccessLayer/Models/FileData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class FileData
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public string ConentType { get; set; }
+        public byte[] FileGuid { get; set; }
+    }
+}
