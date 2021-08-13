c85d1092cbb0ad21f4ad06ffdf488e56d224ceb0
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-14 02:22:08 +0500

    Added file service business logic.

--- BusinessLogicLayer/Services/FileService/FileService.cs
+++ BusinessLogicLayer/Services/FileService/FileService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLayer.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,26 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.Services.FileService
 {
-    public class FileService
+    public class FileService : IFileService
     {
+        public Task<UserFileData> getFile()
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<List<UserFileData>> getFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserFileData> saveFile(UserFileData file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserFileData>> saveFiles(List<UserFileData> files)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
