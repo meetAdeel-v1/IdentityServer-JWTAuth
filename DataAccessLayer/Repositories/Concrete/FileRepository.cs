c65a6a702e83bf1e0b146b408123b06e90ff3b17
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-16 22:46:35 +0500

    Added files Repository.

---
+++ DataAccessLayer/Repositories/Concrete/FileRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Repositories.Interface;
+using DataAccessLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Concrete
+{
+    public class FileRepository : IFileRepository
+    {
+        public Task<UserFileData> getFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserFileData>> getFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserFileData> saveFile(UserFileData file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserFileData>> saveFiles(List<UserFileData> files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
