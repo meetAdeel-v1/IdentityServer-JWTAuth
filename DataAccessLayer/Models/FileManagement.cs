77aca4acced5f65262a212e7b80dcc6c6f39173e
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-16 22:46:01 +0500

    Added file management model.

---
+++ DataAccessLayer/Models/FileManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class FileManagement
+    {
+        [Key]
+        public int Id { get; set; }
+        public int FileId { get; set; }
+        public string UserId { get; set; }
+        public string FileTypeId { get; set; }
+    }
+}
