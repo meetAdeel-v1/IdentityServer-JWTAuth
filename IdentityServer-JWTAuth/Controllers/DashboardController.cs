de3927d7c23436cb60aafe37249d49e72d63d9dc
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-18 02:27:28 +0500

    Added Dashboard controller.

---
+++ IdentityServer-JWTAuth/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer_JWTAuth.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: DashboardController
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        //[Route("getImageFile")]
+        public IActionResult RenderImage(int userId)
+        {
+            //get from db_context
+            //write read into byt[] stream
+            //return File(stream,format,DisplayName);
+            //var stream = new FileStream(@"pathToFile", FileMode.Open);
+            return File("stream", "image/png", "imageName.png");
+        }
+
+    }
+}
