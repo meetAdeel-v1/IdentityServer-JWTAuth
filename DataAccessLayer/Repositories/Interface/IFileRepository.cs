d71a753070b5a096d0ef34e0c453119bf20406d7
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-17 23:57:33 +0500

    Added fileRepository dependency.

---
+++ DataAccessLayer/Repositories/Interface/IFileRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Models;
+using DataAccessLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Interface
+{
+    public interface IFileRepository
+    {
+        Task<FileData> saveFile(UserFileData file);
+        Task<List<FileData>> saveFiles(List<UserFileData> files);
+        Task<FileData> getFile(int fileId);
+        Task<IEnumerable<FileData>> getFiles();
+    }
+}
