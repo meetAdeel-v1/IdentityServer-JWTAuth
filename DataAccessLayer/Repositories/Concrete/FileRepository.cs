52c1ec79a8bc0123e2f1203d70424b4c0e97ab5f
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-25 23:57:16 +0500

    Add Save single file to db logic.

--- DataAccessLayer/Repositories/Concrete/FileRepository.cs
+++ DataAccessLayer/Repositories/Concrete/FileRepository.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Models;
 using DataAccessLayer.Repositories.Interface;
 using DataAccessLayer.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -28,9 +29,21 @@ namespace DataAccessLayer.Repositories.Concrete
             return await _dataContext.Files.ToListAsync();
         }
 
-        public Task<FileData> saveFile(UserFileData file)
+        public async Task<FileData> saveFile(UserFileData file)
         {
-            throw new NotImplementedException();
+            var _file = new FileData();
+            _file.Name = file.filesInfo.Name;
+            _file.FileName = file.filesInfo.FileName;
+            _file.ConentType = file.filesInfo.ConentType;
+            _file.FileGuid = file.filesInfo.FileGuid;
+            _dataContext.Add(_file);
+            var result = await _dataContext.SaveChangesAsync();
+            var _fileManager = new FileManagement();
+            //_fileManager.FileId = file.fileManagerInfo.fileId;
+            _fileManager.FileId = result;
+            _fileManager.UserId = file.fileManagerInfo.UserId;
+            _fileManager.FileTypeId = file.fileManagerInfo.fileType;
+            return _file;
         }
 
         public Task<List<FileData>> saveFiles(List<UserFileData> files)
