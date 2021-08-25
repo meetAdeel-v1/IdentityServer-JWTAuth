11598abb7433d4fe57a1ce897f7f3bccf5ca6125
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-08-25 23:59:17 +0500

    Removed extra namespaces from file repo.

--- DataAccessLayer/Repositories/Concrete/FileRepository.cs
+++ DataAccessLayer/Repositories/Concrete/FileRepository.cs
@@ -2,12 +2,10 @@
 using DataAccessLayer.Models;
 using DataAccessLayer.Repositories.Interface;
 using DataAccessLayer.ViewModels;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories.Concrete
