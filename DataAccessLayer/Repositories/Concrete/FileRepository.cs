a3b3bec040d3866511177547c97f5cec54ed8caa
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-09-25 02:30:05 +0500

    Updated logic in File Repo.

--- DataAccessLayer/Repositories/Concrete/FileRepository.cs
+++ DataAccessLayer/Repositories/Concrete/FileRepository.cs
@@ -17,32 +17,52 @@ namespace DataAccessLayer.Repositories.Concrete
         {
             _dataContext = appContext;
         }
-        public async Task<FileData> getFile(int fileId)
+        public async Task<FileData> getFile(string userId)
         {
-            return await _dataContext.Files.FirstOrDefaultAsync(f=> f.Id==fileId);
+            var result = (from FM in _dataContext.FileManagement
+                          join FD in _dataContext.FileData on FM.FileId equals FD.Id
+                          where FM.UserId == userId && FM.FileTypeId == "ProfileImage"
+                          select new FileData {
+                              Name=FD.Name,
+                              FileName=FD.FileName,
+                              ContentType=FD.ContentType,
+                              FileGuid=FD.FileGuid
+                          }).FirstOrDefault();
+            return result;
         }
 
         public async Task<IEnumerable<FileData>> getFiles()
         {
-            return await _dataContext.Files.ToListAsync();
+            return await _dataContext.FileData.ToListAsync();
         }
 
         public async Task<FileData> saveFile(UserFileData file)
         {
-            var _file = new FileData();
-            _file.Name = file.Name;
-            _file.FileName = file.FileName;
-            _file.ConentType = file.ConentType;
-            _file.FileGuid = file.FileGuid;
-            _dataContext.Add(_file);
-            var result = await _dataContext.SaveChangesAsync();
-            var _fileManager = new FileManagement();
-            //_fileManager.FileId = file.fileManagerInfo.fileId;
-            _fileManager.FileId = result;
-            _fileManager.UserId = file.UserId;
-            _fileManager.FileTypeId = file.fileType;
-            //_dataContext.file
-            return _file;
+            try
+            {
+                var _file = new FileData();
+                _file.Name = file.Name;
+                _file.FileName = file.FileName;
+                _file.ContentType = file.ConentType;
+                _file.FileGuid = file.FileGuid;
+                _dataContext.Add(_file);
+                _dataContext.SaveChanges();
+
+                var result = _file.Id;
+                var _fileManager = new FileManagement();
+                _fileManager.FileId = result;
+                _fileManager.UserId = file.UserId;
+                _fileManager.FileTypeId = file.fileType;
+                _dataContext.Add(_fileManager);
+                _dataContext.SaveChanges();
+
+                return  _file;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
         public Task<List<FileData>> saveFiles(List<UserFileData> files)
