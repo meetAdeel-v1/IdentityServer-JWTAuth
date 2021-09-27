db99590e99f2c6d6db63d0ea7ad72c73e2f57c85
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-09-28 00:03:47 +0500

    Minor issue fixed.

--- DataAccessLayer/Models/FileData.cs
+++ DataAccessLayer/Models/FileData.cs
@@ -13,7 +13,7 @@ namespace DataAccessLayer.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string FileName { get; set; }
-        public string ConentType { get; set; }
+        public string ContentType { get; set; }
         public byte[] FileGuid { get; set; }
     }
 }
