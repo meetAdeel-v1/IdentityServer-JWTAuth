1477d9ec92fced2daa288fd6afd11647cf513194
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-10-01 00:01:33 +0500

    fixed namespace issue.

--- IdentityServer-JWTAuth/Controllers/EmployeeController.cs
+++ IdentityServer-JWTAuth/Controllers/EmployeeController.cs
@@ -1,5 +1,5 @@
 ﻿using DataAccessLayer.Models;
-using IdentityServer_JWTAuth.Models;
+//using IdentityServer_JWTAuth.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
